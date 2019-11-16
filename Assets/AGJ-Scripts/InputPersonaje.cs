31ba091f0ad22cd667f2c8081c50677602702b45
anagonzalezjorge <anagonzalezjorge@gmail.com>
2019-11-16 12:55:41 +0100

    AGJ-Scripts iniciales
    AGJ-Scripts iniciales

---
+++ Assets/AGJ-Scripts/InputPersonaje.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPersonaje : MonoBehaviour
+{
+  
+    //AGJ - Variables para almacenar el valorde la tecla pulsada por el jugador
+
+    [HideInInspector] public float horizontal;     
+    [HideInInspector] public bool jumpHeld;        
+    [HideInInspector] public bool jumpPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AGJ - Procesa los valores de entrada del jugador
+        ProcesaInputs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //AGJ - Acumula las pulsaciones que se realizan entre fotograma y fotograma
+    /*Los valores de GetAxis van de -1 a 1:
+       -1 tecla horizontal izquierda 
+        0 no está pulsando tecla horizontal  
+        1 tecla horizontal derecha*/
+    void ProcesaInputs()
+    {
+        horizontal += Input.GetAxis("Horizontal");
+    }
+    
+
+}
---
+++ Assets/AGJ-Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    InputPersonaje input;                     
+    /*AGJ - Acceder al collider y modificarlo según el movimiento pulsado*/
+    BoxCollider2D bodyCollider;             //The collider component
+    /*AGJ - Referencia para poder aplicar la fuerza par amoverlo o que salte*/
+    Rigidbody2D rigidBody;                  //The rigidbody component
+
+    float jumpTime;                         //Variable to hold jump duration
+    float coyoteTime;                       //Variable to hold coyote duration
+    float playerHeight;                     //Height of the player
+  
+    /*AGJ - originalXScale guarda la escala en X original, para saber
+                                             *si está mirando a la derecha o a la izquierda*/
+    float originalXScale;                   //Original scale on X axis
+    int direction = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
